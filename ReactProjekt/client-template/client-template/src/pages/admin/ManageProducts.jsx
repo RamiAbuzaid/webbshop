f90b145f47b31a1b27d63668c3bd8e54bd71d5ac
Sofie Jägerbo <sofiejagerbo98@gmail.com>
2023-04-18 22:36:06 +0200

    Started manage products page

--- ReactProjekt/client-template/client-template/src/pages/admin/ManageProducts.jsx
+++ ReactProjekt/client-template/client-template/src/pages/admin/ManageProducts.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from "axios";
 
 const ManageProducts = () => {
+const [products, setProducts] = useState([]);
+
+useEffect(() => {
+  fetchProducts()
+}, [])
+
+
+const fetchProducts = async () => {
+  console.log('Hej') 
+  try {
+    //const response = fetch('/api/products')
+    //const response = fetch('')
+    const res = await axios("/api/products");
+    setProducts(res.data);
+    console.log(res);
+    //const data = await response.json();
+    console.log(data)
+  } catch (error) {
+  
+  
+}
+}
+
   return (
+    <div>
     <div>ManageProducts</div>
+
+    <section>
+      {
+        products.map(product => {
+          return <article key={product['_id']}>
+            <p>{product.title}</p>
+
+          </article>
+        })
+      } 
+      
+    </section>
+    </div>
   )
 }
 
 export default ManageProducts
+
